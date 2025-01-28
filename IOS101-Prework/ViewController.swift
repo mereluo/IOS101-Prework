7e6fe906a21ce8d7664d361acf1f94ffa0686f8f
mereluo <meredithluo710@gmail.com>
2025-01-28 10:08:49 -0800

    Initial Commit

---
+++ IOS101-Prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS101-Prework
+//
+//  Created by Meredith Luo on 2025/1/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
